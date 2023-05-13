6eca9859f61725946d6293fe794149217e05ca91
Igor Gusakov <igorgusakov199918@gmail.com>
2023-05-13 07:36:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+const [cardList, setCardList] = useState([
+{id: 1, order:1, text:'Cart 1'},
+{id: 2, order:2, text:'Cart 2'},
+{id: 3, order:3, text:'Cart 3'},
+{id: 4, order:4, text:'Cart 4'},
+])
+const [currentCard, setCurrentCard] = useState({id: 2, order:2, text:'Cart 2'})
+
+const dragStartHandler = (e,card) => {
+setCurrentCard(card)
+console.log(currentCard);
+}
+
+const dragEndHandler = (e) => {
+  e.target.style.background = 'white'
+}
+
+const dragOverHandler = (e) => {
+  e.preventDefault()
+  e.target.style.background = 'red'
+}
+
+const dropHandler = (e,card) => {
+ e.preventDefault()
+ setCardList(cardList.map(c => {
+  if (c.id === card.id){
+    return {...c, order:currentCard.order}
+  }
+  if (c.id === currentCard.id){
+    return {...c, order: card.order}
+  }
+  return c 
+ }));
+ e.target.style.background = 'white'
+}
+
+const sortFunc = (a,b) => {
+  if (a.order > b.order) {
+    return 1;
+  }
+  if (a.order < b.order) {
+    return -1;
+  }
+  return 0;
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      {cardList.sort(sortFunc).map(card =>
+      <div
+      key={card.id}
+      onDragStart={(e) => dragStartHandler(e, card)}
+      onDragLeave={(e) => dragEndHandler(e)}
+      onDragEnd={(e) => dragEndHandler(e)}
+      onDragOver={(e) => dragOverHandler(e)}
+      onDrop={(e) => dropHandler(e,card)} 
+      draggable='true'
+      className='card'>
+         <div>{card.text ? card.text : ''}</div>
+        </div>
+        )}
     </div>
   );
 }
